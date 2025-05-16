5a7e046712f44bb63fcef76b4f215c153a6350fb
7761279b-c739-4152-a1e2-9850adba1b8c <203860514+7761279b-c739-4152-a1e2-9850adba1b8c@users.noreply.github.com>
2025-05-16 16:31:31 +0100

    fix ui

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Company;
 use App\Models\Employee;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
