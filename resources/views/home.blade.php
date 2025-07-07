81733e3600b77cdc9c63632e91fb61b25f8e093d
7761279b-c739-4152-a1e2-9850adba1b8c <203860514+7761279b-c739-4152-a1e2-9850adba1b8c@users.noreply.github.com>
2025-07-07 12:05:54 +0100

    remove default home message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,20 +13,6 @@ $employees = Employee::latest()->simplePaginate(3);
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-            <div class="m-4"></div>
-            <div class="card">
                 <div class="card-header d-flex justify-content-between align-items-center">
                     {{ __('Latest employees') }}
                     <a href="/employee" class="btn btn-secondary">All employees</a>
